a23affcd4e1706352add3470423df1fb339e2fc3
Lobão dos códigos <126257040+Tobias-Costa@users.noreply.github.com>
2023-11-21 17:09:22 -0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,13 +1,16 @@
 //variaveis
-int led1 = 2;
+int led1 = 13;
+int led2 = 10;
+int led3 = 9;
 
 
 //ligando variaveis e porta serial
 void setup() {
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
   Serial.begin(9600);
 }
-
 
 //
 void loop() {
@@ -20,12 +23,35 @@ void loop() {
         digitalWrite(led2, LOW);
         digitalWrite(led3, LOW);
         break;
-      default:
+      case '2':
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3, LOW);
+        break;
+      case '3':
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, HIGH);
+        break;
+      case '4':
         digitalWrite(led1, HIGH);
         digitalWrite(led2, HIGH);
         digitalWrite(led3, HIGH);
         break;
-
+      // case '5':
+      //   digitalWrite(led1, HIGH);
+      //   digitalWrite(led2, HIGH);
+      //   digitalWrite(led3, HIGH);
+      //   delay(500);
+      //   digitalWrite(led1, LOW);
+      //   digitalWrite(led2, LOW);
+      //   digitalWrite(led3, LOW);
+      //   break;
+      case '0':
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, LOW);
+        break;
     }
   }
 }
